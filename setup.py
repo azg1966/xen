729903ebf5b9ba8b92024f523890727aee6068e6
Alex Zener <zener.urt@gmail.com>
2014-12-07 10:18:44 +0300

    setup configured

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+setup(name='Xen',
+      version='0.1',
+      description='Xen App',
+      author='Alex G.',
+      author_email='azx1966@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask'],
      )
